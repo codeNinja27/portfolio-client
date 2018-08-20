10f5834787f1c9c3510b1da837e905856bcceb48
Tharaka Wijekularatne <tharusnow@gmail.com>
2018-08-20 15:30:43 -0700

    added logos for footer

--- src/components/footer-page.js
+++ src/components/footer-page.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './footer-page.css';
+import gitHubLogo from '../images/github.png';
+import linkedInLogo from '../images/linkedIn.png';
+import gmailLogo from '../images/gmailLogo.png';
 
 export class FooterBar extends React.Component {
 
     render() {
         return (
             <div className="footer-bar">
-                <a href="https://github.com/codeNinja27" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="https://www.linkedin.com/in/tharakawije/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                <a href="mailto:tharakawijekularatne@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
+                <div className="gitHubDiv">
+                  <a href="https://github.com/codeNinja27" target="_blank" rel="noopener noreferrer"><img src={ gitHubLogo } alt="gitHub logo" /></a>
+                </div>
+                <div className="linkedInDiv">
+                  <a href="https://www.linkedin.com/in/tharakawije/" target="_blank" rel="noopener noreferrer"><img src={ linkedInLogo } alt="linkedIn logo" /></a>
+                </div>
+                <div className="emailDiv">
+                  <a href="mailto:tharakawijekularatne@gmail.com" target="_blank" rel="noopener noreferrer"><img src={ gmailLogo } alt="gmail logo" /></a>
+                </div>
             </div>
         );
     }
