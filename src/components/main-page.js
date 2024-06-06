1275fe107ac941a8d189c4a4502acc41e85c7eee
Tharaka Wijekularatne <tharusnow@gmail.com>
2024-06-05 20:26:45 -0700

    more style changes

--- src/components/main-page.js
+++ src/components/main-page.js
@@ -5,7 +5,21 @@ import './main-page.css';
 export function mainPage(props) {
     return (
         <div className="mainPage">    
-  
+
+            <div className="project bg-light">
+              <h2 className="projectTitle">Discord Bot</h2>
+              <div className="img img2">
+              </div>
+              <div className="projectDes">
+                A muslti-purpose discord bot that inspire, give you inspiring qoutes from an api, and play music for you!
+              </div>
+              <div className="techStack">
+                  Python | PostgreSQL | API
+              </div>
+              <a className="live2 btn btn-outline-primary" href="" rel="noopener noreferrer">Live App</a>
+              <a className="repo btn btn-outline-primary" href="https://github.com/codeNinja27/DiscordBot" target="_blank" rel="noopener noreferrer">Project Repo</a>   
+            </div>
+
             <div className="project bg-light">
               <h2 className="projectTitle">Odin Recipes</h2>
               <div className="img img3">
@@ -21,20 +35,6 @@ export function mainPage(props) {
             </div>
 
             <div className="project bg-light">
-              <h2 className="projectTitle">Discord Bot</h2>
-              <div className="img img2">
-              </div>
-              <div className="projectDes">
-                A muslti-purpose discord bot that inspire, give you inspiring qoutes from an api, and play music for you!
-              </div>
-              <div className="techStack">
-                  Python | PostgreSQL | API
-              </div>
-              <a className="live btn btn-outline-primary" href="" target="_blank" rel="noopener noreferrer">Live App</a>
-              <a className="repo btn btn-outline-primary" href="https://github.com/codeNinja27/DiscordBot" target="_blank" rel="noopener noreferrer">Project Repo</a>   
-            </div>
-
-            <div className="project bg-light">
               <h2 className="projectTitle" >Calculator Interface</h2>
               <div className="img img1">
               </div>
@@ -44,7 +44,7 @@ export function mainPage(props) {
               <div className="techStack">
                 C++ | HTML | CSS 
               </div>
-              <a className="live btn btn-outline-primary" href="" target="_blank" rel="noopener noreferrer">Live App</a>
+              <a className="live2 btn btn-outline-primary" href="#" rel="noopener noreferrer">Live App</a>
               <a className="repo btn btn-outline-primary" href="https://github.com/codeNinja27/calculator" target="_blank" rel="noopener noreferrer">Project Repo</a>
             </div>  
 
